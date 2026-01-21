6d0e8f195a8f1893be824754e01da2d76240ff08
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-21 16:01:49 +0000

    CH2L1: add textnode creation

---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        for key, val in self:
+            if not val == other[key]:
+                return False
+        return True
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
