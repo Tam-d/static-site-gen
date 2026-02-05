26e76489970cede99fd98d56fca3c11c84950a93
Tam <5542463+Tam-d@users.noreply.github.com>
2026-02-05 02:45:07 +0000

    Fix image leaf node props

--- src/textnode.py
+++ src/textnode.py
@@ -22,7 +22,7 @@ def text_node_to_html_node(text_node):
         case TextType.LINK:
             return LeafNode("a", text_node.text, {"href": f"{text_node.url}" })
         case TextType.IMAGE:
-            return LeafNode("img", "", {"alt": f"{text_node.text}"})
+            return LeafNode("img", "", {"src": f"{text_node.url}","alt": f"{text_node.text}"})
         case _:
             raise Exception("Text node's type is not valid")
         
