215462225dc72b92b9431a9bdf7a48bbd919c8a5
Tam <5542463+Tam-d@users.noreply.github.com>
2026-02-05 02:52:27 +0000

    Fix incorrect logic for leaf node html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,9 +29,10 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
-            raise ValueError()
-        if not self.tag:
+
+        if self.value is None:
+            raise ValueError("Leaf node has no value")
+        if self.tag is None:
             return self.value
         
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
